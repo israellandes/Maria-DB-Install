f20b3be5e77fcbcb69e011802933e82acd4b52c8
Ubuntu <ubuntu@ip-172-31-30-9.us-west-1.compute.internal>
2023-09-16 19:15:22 +0000

    Good?

--- bin/set_password.sh
+++ bin/set_password.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+# Prompt for the MariaDB root password
 read -s -p "Enter the MariaDB root password: " MARIADB_PASSWORD
+echo
+
+# Prompt for password confirmation
+read -s -p "Confirm the MariaDB root password: " CONFIRM_PASSWORD
+echo
+
+# Check if passwords match
+if [ "$MARIADB_PASSWORD" != "$CONFIRM_PASSWORD" ]; then
+    echo "Passwords do not match. Please try again."
+    exit 1
+fi
 
 export MARIADB_PASSWORD
 
